4148d79b894aaeceac6770690697889f9d349e9e
shub-sanni <shubhamsanni8986@gmail.com>
2026-08-17 16:41:10 +0530

    git practice on Gitproj repo

--- src/main/java/utilities/ExtentTestManager.java
+++ src/main/java/utilities/ExtentTestManager.java
@@ -1,15 +1,15 @@
-//package utilities;
-//
-//import com.aventstack.extentreports.ExtentTest;
-//
-//public class ExtentTestManager {
-//private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
-//
-//	public static ExtentTest getTest() {
-//		return test.get();
-//	}
-//
-//	public static void setTest(ExtentTest extentTest) {
-//		test.set(extentTest);
-//	}
-//}
+package utilities;
+
+import com.aventstack.extentreports.ExtentTest;
+
+public class ExtentTestManager {
+private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+	public static ExtentTest getTest() {
+		return test.get();
+	}
+
+	public static void setTest(ExtentTest extentTest) {
+		test.set(extentTest);
+	}
+}
